61a02390ab61086f9153d5ad9f96e70334b821c1
y1singh <y1singh@eng.ucsd.edu>
2020-06-11 21:35:00 -0700

    Committing the webapp
    Adding different tabs for the webapp like vgg page and resnet page and the cover.css for styling. Also, adding changes to server.py to render the same.

--- controller.py
+++ controller.py
@@ -1,11 +1,13 @@
 import cv2
+from classification import ImageClassifier
 
 class VideoCamera():
-    def __init__(self):
-        from classification import ImageClassifier
+    def __init__(self,model_name):
+        
+        assert(model_name=="vgg" or model_name=="wide_resnet")
         
         self.video = cv2.VideoCapture(0)
-        self.classifier = ImageClassifier('wide_resnet')
+        self.classifier = ImageClassifier(model_name)
 
     def __del__(self):
         self.video.release()   
@@ -13,7 +15,7 @@ class VideoCamera():
     def get_frame(self):
         ret, frame = self.video.read()
 
-        # DO WHAT YOU WANT WITH TENSORFLOW / KERAS AND OPENCV
+        # Perform bounding box detection and classification
         jpeg = self.classifier.classify_image(frame)
         ret, jpeg = cv2.imencode('.jpg',jpeg)
 
--- server.py
+++ server.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, Response, jsonify
 from controller import VideoCamera
 import cv2
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
 
-video_stream = VideoCamera()
+video_stream_vgg = VideoCamera("vgg")
+video_stream_resnet = VideoCamera("wide_resnet")
 
 @app.route('/')
 def index():
@@ -16,9 +19,22 @@ def gen(camera):
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen(video_stream),
+@app.route('/video_feed_vgg')
+def video_feed_vgg():
+    return Response(gen(video_stream_vgg),
+                mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/vgg_page')
+def vgg_page():
+    return render_template('./vgg_page.html')
+
+@app.route('/resnet_page')
+def resnet_page():
+    return render_template('./resnet_page.html')
+
+@app.route('/video_feed_resnet')
+def video_feed_resnet():
+    return Response(gen(video_stream_resnet),
                 mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
